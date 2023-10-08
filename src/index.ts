069ab3bb8960bf643715578c280e09fa46dff59a
ColonelSandvich <21989418+Colonel-Sandvich@users.noreply.github.com>
2023-10-08 03:18:07 +0100

    Add export for createH3ApiHandler

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { createNuxtApiHandler } from "./apiHandler.ts";
+export { createNuxtApiHandler, createH3ApiHandler } from "./apiHandler.ts";
 export { createTrpcVueClient } from "./client.ts";
 export { customFetchWrapper } from "./customFetch.ts";
